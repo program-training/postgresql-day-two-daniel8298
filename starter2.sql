43cd9e7d04fcc581f09797524b5d813997193e6c
daniel8298 <daniel8298@gmail.com>
2023-09-13 16:26:05 +0300

    sql_ex2_B

--- starter2.sql
+++ starter2.sql
@@ -24,21 +24,21 @@ INSERT INTO university.teacherCourse (teacher_id, course_id)
 VALUES (1,1),(2,2),(3,3);
 
 
+
+
 CREATE TABLE university.grades(
 grades_id SERIAL PRIMARY KEY,
 grade INT,
 CHECK (grade BETWEEN 0 AND 100),
-student_id INT,
-FOREIGN KEY (student_id) REFERENCES university.students(student_id),
-course_id INT,
-FOREIGN KEY (course_id) REFERENCES university.courses(course_id)
-
+enrollment_id INT,
+FOREIGN KEY (enrollment_id) REFERENCES university.enrollments(enrollment_id)
 );
 
 
 
-INSERT INTO university.grades (grade,student_id, course_id)
-VALUES (98,1, 1),(88,2,1),(100,3,2),(92,4,3),(99,1,3)
+
+INSERT INTO university.grades (enrollment_id,grade)
+VALUES (1,98),(2,88),(3,100),(4,92),(1,99)
 
 
 
